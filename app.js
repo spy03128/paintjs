01a9567c0e9886776be4d06c030c0d06b4b67f5a
지현 <57143818+spy03128@users.noreply.github.com>
2021-01-01 23:34:39 +0900

    Draw line

---
+++ app.js
@@ -0,0 +1,37 @@
+const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
+
+canvas.width = 700;
+canvas.height = 700;
+
+ctx.strokeStyle = "#2c2c2c"; //default = black
+ctx.linewidth = 2.5; //선의굵기
+
+let painting = false;
+
+function stopPainting() {
+  painting = false;
+}
+
+function startPainting() {
+  painting = true;
+}
+
+function onMouseMove(event) {
+  const x = event.offsetX; //x좌표
+  const y = event.offsetY; //y좌표
+  if (!painting) {
+    ctx.beginPath(); //path = line
+    ctx.moveTo(x, y);
+  } else {
+    ctx.lineTo(x, y);
+    ctx.stroke();
+  }
+}
+
+if (canvas) {
+  canvas.addEventListener("mousemove", onMouseMove);
+  canvas.addEventListener("mousedown", startPainting);
+  canvas.addEventListener("mouseup", stopPainting);
+  canvas.addEventListener("mouseleave", stopPainting);
+}
